73c9736aa74467b52358487f45b41d2d509661a1
Danil-Malkin-NN <53613490+Danil-Malkin-NN@users.noreply.github.com>
2021-02-28 15:39:42 +0300

    add config file.

---
+++ src/main/java/develop/Marvel/config/MainConfig.java
@@ -0,0 +1,11 @@
+package develop.Marvel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/default.properties")
+@PropertySource(value = "classpath:/local.properties")
+public class MainConfig {
+
+}
